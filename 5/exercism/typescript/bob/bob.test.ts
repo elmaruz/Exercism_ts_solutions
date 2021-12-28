d701ab0485e4d01dfe81bdae40ec2f945b4e3bef
elmaruz <lmarussig@gmail.com>
2021-12-28 15:06:00 +0000

    (28.12) Bob - 20/24

--- 5/exercism/typescript/bob/bob.test.ts
+++ 5/exercism/typescript/bob/bob.test.ts
@@ -6,32 +6,32 @@ describe('Bob', () => {
     expect(result).toEqual('Whatever.')
   })
 
-  xit('shouting', () => {
+  it('shouting', () => {
     const result = hey('WATCH OUT!')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('shouting gibberish', () => {
+  it('shouting gibberish', () => {
     const result = hey('FCECDFCAAB')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('asking a question', () => {
+  it('asking a question', () => {
     const result = hey('Does this cryogenic chamber make me look fat?')
     expect(result).toEqual('Sure.')
   })
 
-  xit('asking a numeric question', () => {
+  it('asking a numeric question', () => {
     const result = hey('You are, what, like 15?')
     expect(result).toEqual('Sure.')
   })
 
-  xit('asking gibberish', () => {
+  it('asking gibberish', () => {
     const result = hey('fffbbcbeab?')
     expect(result).toEqual('Sure.')
   })
 
-  xit('talking forcefully', () => {
+  it('talking forcefully', () => {
     const result = hey("Let's go make out behind the gym!")
     expect(result).toEqual('Whatever.')
   })
@@ -41,37 +41,37 @@ describe('Bob', () => {
     expect(result).toEqual('Whatever.')
   })
 
-  xit('forceful question', () => {
+  it('forceful question', () => {
     const result = hey('WHAT THE HELL WERE YOU THINKING?')
     expect(result).toEqual("Calm down, I know what I'm doing!")
   })
 
-  xit('shouting numbers', () => {
+  it('shouting numbers', () => {
     const result = hey('1, 2, 3 GO!')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('no letters', () => {
+  it('no letters', () => {
     const result = hey('1, 2, 3')
     expect(result).toEqual('Whatever.')
   })
 
-  xit('question with no letters', () => {
+  it('question with no letters', () => {
     const result = hey('4?')
     expect(result).toEqual('Sure.')
   })
 
-  xit('shouting with special characters', () => {
+  it('shouting with special characters', () => {
     const result = hey('ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('shouting with no exclamation mark', () => {
+  it('shouting with no exclamation mark', () => {
     const result = hey('I HATE THE DMV')
     expect(result).toEqual('Whoa, chill out!')
   })
 
-  xit('statement containing question mark', () => {
+  it('statement containing question mark', () => {
     const result = hey('Ending with ? means a question.')
     expect(result).toEqual('Whatever.')
   })
@@ -81,37 +81,37 @@ describe('Bob', () => {
     expect(result).toEqual('Sure.')
   })
 
-  xit('silence', () => {
+  it('silence', () => {
     const result = hey('')
     expect(result).toEqual('Fine. Be that way!')
   })
 
-  xit('prolonged silence', () => {
+  it('prolonged silence', () => {
     const result = hey('   ')
     expect(result).toEqual('Fine. Be that way!')
   })
 
-  xit('alternate silence', () => {
+  it('alternate silence', () => {
     const result = hey('\t\t\t\t\t\t\t\t\t\t')
     expect(result).toEqual('Fine. Be that way!')
   })
 
-  xit('multiple line question', () => {
+  it('multiple line question', () => {
     const result = hey('\nDoes this cryogenic chamber make me look fat?\nNo.')
     expect(result).toEqual('Whatever.')
   })
 
-  xit('starting with whitespace', () => {
+  it('starting with whitespace', () => {
     const result = hey('         hmmmmmmm...')
     expect(result).toEqual('Whatever.')
   })
 
-  xit('ending with whitespace', () => {
+  it('ending with whitespace', () => {
     const result = hey('Okay if like my  spacebar  quite a bit?   ')
     expect(result).toEqual('Sure.')
   })
 
-  xit('other whitespace', () => {
+  it('other whitespace', () => {
     const result = hey('\n\r \t')
     expect(result).toEqual('Fine. Be that way!')
   })
--- 5/exercism/typescript/bob/bob.ts
+++ 5/exercism/typescript/bob/bob.ts
@@ -1,3 +1,21 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function hey(message: string): string {
+  message = message.trim();
+  
+  if (message !== message.toUpperCase() && message.match(/(?:[^"']|^)(\w\.|!|'')()(?!["'])/g)) {
+    return 'Whatever.';
+  }
+  if (message.match(/(?:[^"']|^)([a-z]\?|[0-9]\?)(?!["'])/g)) {
+    return 'Sure.';
+  }
+  if (message.match(/(?:[^"']|^)([A-Z]\?)(?!["'])/g)) {
+    return `Calm down, I know what I'm doing!`;
+  }
+  if (message === message.toUpperCase() && message.match(/(?:[^"']|^)(\w\!*)(?!["'])/g)) {
+    return `Whoa, chill out!`;
+  }
+  if (!message) {
+    return 'Fine. Be that way!';
+  }
+
+  throw new Error('dunno.')
 }
